0d1f4ae7962085da99e758d09cad7fb5fb7e5f93
Sven Gehring <cbrxde@gmail.com>
2021-09-11 18:58:06 +0200

    add seed data with all rules from the exam sheet

--- DB/Migrations/20210911131244_SeedDataRules.Designer.cs
+++ DB/Migrations/20210911131244_SeedDataRules.Designer.cs
@@ -4,14 +4,16 @@ using DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DB.Migrations
 {
     [DbContext(typeof(TIAE6Context))]
-    partial class TIAE6ContextModelSnapshot : ModelSnapshot
+    [Migration("20210911131244_SeedDataRules")]
+    partial class SeedDataRules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DB/Migrations/20210911131244_SeedDataRules.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace DB.Migrations
+{
+    public partial class SeedDataRules : Migration
+    {
+        private DateTime fakeDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified);
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+              migrationBuilder.InsertData(
+              table: "Rule",
+              columns: new[] { "id", "parentId", "Discriminator", "rule", "createdAt", "modifiedAt", "condition", "transformation" },
+              values: new object[,]
+              {
+                  { 1, null, "InferenceRule", "Inferenz-Root", this.fakeDate, this.fakeDate, "true", "" },
+                  { 2, 1, "InferenceRule", "Inferenz-Vermoegen", this.fakeDate, this.fakeDate, "lj_vermoegen == null || lj_vermoegen == 0", "lj_vermoegen = lj_einkommen * 0.75" },
+
+                  { 3, null, "EvaluationRule", "Evaluation-Root", this.fakeDate, this.fakeDate, "true", "" },
+                  { 4, 3, "EvaluationRule", "Fall 4", this.fakeDate, this.fakeDate, "vj_einkommen > (1.5* lj_einkommen) && vj_einkommen > 100000", "lj_flagged = true" },
+                  { 5, 3, "EvaluationRule", "Knoten", this.fakeDate, this.fakeDate, "true", "" },
+                  { 6, 5, "EvaluationRule", "VM/EK < 1", this.fakeDate, this.fakeDate, "(lj_vermoegen / lj_einkommen) < 1", "" },
+                  { 7, 6, "EvaluationRule", "Fall 1", this.fakeDate, this.fakeDate, "lj_einkommen < 80000", "lj_steuersatz = lj_einkommen * 0.25" },
+                  { 8, 6, "EvaluationRule", "Fall 3", this.fakeDate, this.fakeDate, "lj_einkommen > 100000", "lj_flagged = true"},
+                  { 9, 5, "EvaluationRule", "VM/EK > 1", this.fakeDate, this.fakeDate, "(lj_vermoegen / lj_einkommen) > 1", "" },
+                  { 10, 9, "EvaluationRule", "Fall 2", this.fakeDate, this.fakeDate, "lj_einkommen > 100000", "lj_steuersatz = lj_einkommen * 0.35" }
+              });
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

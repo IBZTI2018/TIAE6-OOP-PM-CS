e5f40641eba5d14114ac2325e51984494df68853
Sven Gehring <cbrxde@gmail.com>
2021-09-19 20:24:06 +0200

    fix wrong variables

--- DB/Migrations/20210911131244_SeedDataRules.cs
+++ DB/Migrations/20210911131244_SeedDataRules.cs
@@ -18,11 +18,11 @@ namespace DB.Migrations
                   { 2, 1, "InferenceRule", "Fall 5", this.fakeDate, this.fakeDate, "lj_vermoegen == null || lj_vermoegen == 0", "lj_vermoegen = lj_einkommen * 0.75" },
 
                   { 3, null, "EvaluationRule", "Evaluation-Root", this.fakeDate, this.fakeDate, "true", "" },
-                  { 4, 3, "EvaluationRule", "Fall 4", this.fakeDate, this.fakeDate, "vj_einkommen > (1.5* lj_einkommen) && vj_einkommen > 100000", "lj_flagged = true" },
+                  { 4, 3, "EvaluationRule", "Fall 4", this.fakeDate, this.fakeDate, "vj_einkommen > (1.5* lj_einkommen) && vj_einkommen > 100000", "lj_warnung = true" },
                   { 5, 3, "EvaluationRule", "Knoten", this.fakeDate, this.fakeDate, "true", "" },
                   { 6, 5, "EvaluationRule", "VM/EK < 1", this.fakeDate, this.fakeDate, "(lj_vermoegen / lj_einkommen) < 1", "" },
                   { 7, 6, "EvaluationRule", "Fall 1", this.fakeDate, this.fakeDate, "lj_einkommen < 80000", "lj_steuersatz = lj_einkommen * 0.25" },
-                  { 8, 6, "EvaluationRule", "Fall 3", this.fakeDate, this.fakeDate, "lj_einkommen > 100000", "lj_flagged = true"},
+                  { 8, 6, "EvaluationRule", "Fall 3", this.fakeDate, this.fakeDate, "lj_einkommen > 100000", "lj_warnung = true"},
                   { 9, 5, "EvaluationRule", "VM/EK > 1", this.fakeDate, this.fakeDate, "(lj_vermoegen / lj_einkommen) > 1", "" },
                   { 10, 9, "EvaluationRule", "Fall 2", this.fakeDate, this.fakeDate, "lj_einkommen > 100000", "lj_steuersatz = lj_einkommen * 0.35" }
               });
